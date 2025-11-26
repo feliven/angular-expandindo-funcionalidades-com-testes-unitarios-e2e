20bfb3f3da97158fd124b73836b9de342360e3b2
FVS <feliven@gmail.com>
2025-11-26 12:48:18 -0300

    criados testes para back-button directive

--- src/app/shared/back-button/back-button.directive.spec.ts
+++ src/app/shared/back-button/back-button.directive.spec.ts
@@ -1,9 +1,31 @@
 import { BackButtonDirective } from './back-button.directive';
-import { expect } from '@jest/globals';
+import { Location } from '@angular/common';
+import { expect, jest } from '@jest/globals';
 
 describe('BackButtonDirective', () => {
+  let directive: BackButtonDirective;
+  let mockLocation: jest.Mocked<Location>;
+
+  beforeEach(() => {
+    mockLocation = {
+      back: jest.fn(),
+    } as any;
+    directive = new BackButtonDirective(mockLocation);
+  });
+
   it('should create an instance', () => {
-    const directive = new BackButtonDirective();
     expect(directive).toBeTruthy();
+  });
+
+  it('should call location.back() when onClick is called', () => {
+    directive.onClick();
+    expect(mockLocation.back).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call location.back() multiple times when onClick is called multiple times', () => {
+    directive.onClick();
+    directive.onClick();
+    directive.onClick();
+    expect(mockLocation.back).toHaveBeenCalledTimes(3);
   });
 });
