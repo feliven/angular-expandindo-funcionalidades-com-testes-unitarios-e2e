1c7378bebe711e29a3c36aa01ca652e96b6e1215
FVS-MBA <feliven@gmail.com>
2025-11-25 21:19:49 -0300

    criados testes para pagina-nao-encontrada

--- src/app/core/erro/pagina-nao-encontrada/pagina-nao-encontrada.component.spec.ts
+++ src/app/core/erro/pagina-nao-encontrada/pagina-nao-encontrada.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('PaginaNaoEncontradaComponent', () => {
   let component: PaginaNaoEncontradaComponent;
@@ -36,4 +38,38 @@ describe('PaginaNaoEncontradaComponent', () => {
   it('should initialize component properties', () => {
     expect(component instanceof PaginaNaoEncontradaComponent).toBe(true);
   });
+
+  it('deveria carregar app-banner 2 vezes', () => {
+    const banners: DebugElement[] = fixture.debugElement.queryAll(
+      By.css('app-banner')
+    );
+    expect(banners.length).toBe(2);
+  });
+
+  it('deveria carregar app-container 1 vez', () => {
+    const container: DebugElement = fixture.debugElement.query(
+      By.css('app-container')
+    );
+    expect(container).toBeTruthy();
+  });
+
+  it('deveria carregar section 1 vez', () => {
+    const section: DebugElement = fixture.debugElement.query(By.css('section'));
+    expect(section).toBeTruthy();
+  });
+
+  it('should load paragraph with text Página não encontrada', () => {
+    const paragrafo: DebugElement = fixture.debugElement.query(By.css('p'));
+    expect(paragrafo.nativeElement.textContent).toContain(
+      'Página não encontrada'
+    );
+  });
+
+  it('should load link with text inicial to homepage', () => {
+    const link: DebugElement = fixture.debugElement.query(By.css('a'));
+    expect(link.nativeElement.textContent).toContain('inicial');
+    expect(link.nativeElement.getAttribute('routerLink')).toBe('/');
+  });
+
+  // it("",()=>{})
 });
